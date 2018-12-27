a5aeb74012331b2f4912d3f9f7bb68ec5d07298c
James Quick <james.q.quick@outlook.com>
2018-12-27 11:07:07 -0600

    Testing netlify functions

---
+++ src/pages/success.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Success = props => (
+  <Layout>
+    <h1>Thanks for reaching out!</h1>
+  </Layout>
+);
+
+export default Success;
