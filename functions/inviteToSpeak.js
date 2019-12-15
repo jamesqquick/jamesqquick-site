29a5ce84d0c033c608a62c6a193ac821d81bd1ca
James Quick <james.q.quick@outlook.com>
2019-12-15 13:40:35 -0600

    added nodemailer to send email from invite me to speak

--- functions/inviteToSpeak.js
+++ functions/inviteToSpeak.js
@@ -1,11 +1,37 @@
-const axios = require("axios");
 require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 exports.handler = function(event, context, callback) {
-  const parsedBody = JSON.parse(event.body);
-  console.log(parsedBody);
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: "Email sent" }),
+  const { email, name, body } = JSON.parse(event.body);
+
+  const mailOptions = {
+    from: email,
+    to: process.env.ADMIN_EMAIL,
+    subject: "Someone wants you to speak!",
+    text: `${body} \n from ${name}`,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.error(error);
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ msg: "Failed to send email." }),
+      });
+    } else {
+      console.log(info);
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ msg: "Email sent" }),
+      });
+    }
   });
 };
