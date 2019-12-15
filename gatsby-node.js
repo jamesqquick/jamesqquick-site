451b3348621a94cbc68808936083b04f16efa5d4
James Quick <james.q.quick@outlook.com>
2019-12-15 11:24:14 -0600

    Renamed components to uppercase

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const talkPage = path.resolve("./src/pages/Talk.js");
+  const talkPage = path.resolve("./src/pages/talk.js");
 
   const talksResult = await graphql(
     `
@@ -71,7 +71,7 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 
-  const postPage = path.resolve("./src/pages/BlogPost.js");
+  const postPage = path.resolve("./src/pages/blogPost.js");
 
   const postsResult = await graphql(
     `
