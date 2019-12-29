8b0eb0972e0fb38891749adccaac83b53299ad3c
James Quick <james.q.quick@outlook.com>
2019-12-29 14:55:51 -0600

    Added empty files for resources and uses pages

---
+++ src/pages/resources.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function resources() {
+  return (
+    <div>
+      <p>Here's my resources</p>
+    </div>
+  );
+}
---
+++ src/pages/uses.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function uses() {
+  return (
+    <div>
+      <p>Here's what I use :)</p>
+    </div>
+  );
+}
