c04ad3534bd202d434de949a48288ed1d753fd63
James Quick <james.q.quick@outlook.com>
2020-01-02 12:46:41 -0600

    gatsby image for speaking page

--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -1,21 +1,61 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import TalkPreview from "../components/TalkPreview";
 import ContactBlurb from "../components/ContactBlurb";
+import Img from "gatsby-image";
 
-export default function talks({ data }) {
-  const talks = data.allTalksJson.edges.map(item => item.node);
+export default function talks() {
+  const talksData = useStaticQuery(graphql`
+    {
+      allTalksJson(sort: { fields: date, order: DESC }) {
+        edges {
+          node {
+            title
+            conference
+            slug
+            description
+            imageUrl
+            date
+            id
+            slidesLink
+          }
+        }
+      }
+    }
+  `);
+  const talks = talksData.allTalksJson.edges.map(item => item.node);
   const blurbHeader = "Intersted in me speaking at your event?";
-
+  const speakingPicture = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "speaking.jpg" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
       <SEO title="Speaking" keywords={[`conference talks`]} />
       <div className="container">
         <h1 className="text-center section-title">Speaking</h1>
         <hr className="title-underline" />
+        <p>
+          I began my speaking career as a Technical Evangelist at Microsoft and
+          have loved it ever since. I’ve spoken and mentored at events across
+          the nation and am always looking for new opportunities.
+        </p>
         <ContactBlurb header={blurbHeader} />
+        <Img
+          fluid={speakingPicture.file.childImageSharp.fluid}
+          alt="James Q Quick speaking"
+        />
         {talks.map(talk => (
           <TalkPreview key={talk.id} talk={talk}></TalkPreview>
         ))}
@@ -23,22 +63,3 @@ export default function talks({ data }) {
     </Layout>
   );
 }
-
-export const query = graphql`
-  {
-    allTalksJson(sort: { fields: date, order: DESC }) {
-      edges {
-        node {
-          title
-          conference
-          slug
-          description
-          imageUrl
-          date
-          id
-          slidesLink
-        }
-      }
-    }
-  }
-`;
