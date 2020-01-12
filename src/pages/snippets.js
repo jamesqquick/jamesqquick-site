c4e2ec46cfe14dd54e63d173a4a6ee029d5d003e
James Quick <james.q.quick@outlook.com>
2020-01-11 22:08:41 -0600

    added a bit of content to snippets page

--- src/pages/snippets.js
+++ src/pages/snippets.js
@@ -2,7 +2,9 @@ import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-export default function snippets() {
+export default function snippets({ data }) {
+  const content = data.allMarkdownRemark.edges[0].node.html;
+
   return (
     <Layout>
       <SEO
@@ -15,11 +17,26 @@ export default function snippets() {
         ]}
       />
       <div className="container">
-        <h1 className="title">Snippets</h1>
-        <hr className="title-underline" />
-        <p>Here are the snippets I use in Visual Studio Code</p>
-        <h2>JavaScript</h2>
+        <div className="post">
+          <h1 className="title">Snippets</h1>
+          <hr className="title-underline" />
+          <p>Here are some helpful snippets I use in Visual Studio Code</p>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
       </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query SnippetsQuery {
+    allMarkdownRemark(filter: { frontmatter: { title: { eq: "Snippets" } } }) {
+      edges {
+        node {
+          id
+          html
+        }
+      }
+    }
+  }
+`;
