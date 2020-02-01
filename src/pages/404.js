74a0cb793fd21f9c7469d8f0daa6b051ea7a949e
James Quick <james.q.quick@outlook.com>
2020-02-01 11:09:47 -0600

    Wrapping 404 in container

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/SEO";
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 );
 
