032e8f8b24df7bf20547a04ac89ae21cc45b431a
jamesqquick <james.q.quick@outlook.com>
2020-02-04 08:22:26 -0600

    Trying out sendgrid for email

--- functions/contact.js
+++ functions/contact.js
@@ -1,36 +1,31 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.ADMIN_EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = function(event, context, callback) {
   const { email, name, body, type } = JSON.parse(event.body);
-  const mailOptions = {
-    from: email,
+
+  const msg = {
     to: process.env.ADMIN_EMAIL,
-    subject: `${type} request`,
+    from: email,
+    subject: type ? `${type} request` : "Mail from JamesQQuick.com",
     text: `${body} \n from ${name}`,
+    html: `${body} \n from ${name}`,
   };
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.error(error);
-      return callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({ msg: "Failed to send email." }),
-      });
-    } else {
+  sgMail
+    .send(msg)
+    .then(res => {
       console.log(info);
       return callback(null, {
         statusCode: 200,
         body: JSON.stringify({ msg: "Email sent" }),
       });
-    }
-  });
+    })
+    .catch(err => {
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ msg: "Failed to send email." }),
+      });
+    });
 };
