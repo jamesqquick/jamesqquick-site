030ecdcc15ca24855aa3694ce357df86aad42008
James Quick <james.q.quick@outlook.com>
2020-06-11 08:13:00 -0500

    Updated code styling with dark mode from prism

--- src/serializers.js
+++ src/serializers.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Img from "gatsby-image";
 import { getFluidGatsbyImage } from "gatsby-source-sanity";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const sanityConfig = { projectId: "rx426fbd", dataset: "production" };
 
 export default {
   types: {
     code: props => (
-      <SyntaxHighlighter language={props.node.language || "text"}>
+      <SyntaxHighlighter
+        language={props.node.language || "text"}
+        style={atomDark}
+        className="pre"
+      >
         {props.node.code}
       </SyntaxHighlighter>
     ),
