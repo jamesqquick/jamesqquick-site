9a4b8aa90445db2d7670ff279aa993f40537fec8
James Quick <james.q.quick@outlook.com>
2020-07-31 08:23:51 -0500

    Added resources to sanity

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import { graphql } from "gatsby";
 
-export default function resources() {
+export default function resources({ data }) {
+  const resources = data.allSanityResource.nodes;
+  const types = new Set();
+  for (let i = 0; i < resources.length; i++) {
+    const resource = resources[i];
+    types.add(resource.type[0].title);
+  }
+  const typesArr = [...types];
+  console.log(typesArr);
+  console.log(resources);
   return (
     <Layout>
       <SEO
@@ -28,460 +38,45 @@ export default function resources() {
           of the content listed below or had close friends share very positive
           feedback.
         </p>
-        <h2>Paid Courses</h2>
-        <h3 className="h3">JavaScript</h3>
-        <ul>
-          <li>
-            <a
-              href="https://BeginnerJavaScript.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Beginner JavaScript by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://ES6.io/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ES6 For Everyone by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/understand-javascript/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JavaScript: Understanding the Weird Parts by Anthony Alicea
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/modern-javascript/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              The Modern JavaScript Bootcamp by Andrew Percival
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/javascript-advanced/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Advanced JavaScript by Asim Hussain
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">React</h3>
-        <ul>
-          <li>
-            <a
-              href="https://AdvancedReact.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Advanced React by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://ReactForBeginners.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React for Beginners by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React - The Complete Guide by Maximilian Schwarzmüller
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/react-2nd-edition/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              The Complete React Developer Course by Andrew Percival
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/the-complete-react-native-and-redux-course/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              The Complete React Native Course by Stephen Grider
-            </a>
-          </li>
-          <li>
-            <a href="https://www.udemy.com/course/node-with-react-fullstack-web-development/">
-              Node with React by Stephen Grider
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/mern-stack-front-to-back/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              MERN Stack Front To Back by Brad Traversy
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">Backend</h3>
-        <ul>
-          <li>
-            <a
-              href="https://LearnNode.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Node by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/the-complete-nodejs-developer-course-2/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              The Complete Nodejs Developer Course by Andrew Percival
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/understand-nodejs/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn and Understand Nodejs by Andrew Alicea
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/nodejs-the-complete-guide/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Nodejs the Complete Guide by Maximilian Schwarzmüller
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">CSS</h3>
-        <ul>
-          <li>
-            <a
-              href="https://www.udemy.com/course/practical-css-grid/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Practical CSS Grid by Bryan Robinson
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/modern-html-css-from-the-beginning/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Modern HTML and CSS From the Beginning by Brad Traversy
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.udemy.com/course/css-the-complete-guide-incl-flexbox-grid-sass/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CSS The Complete Guide by Maximilian Schwarzmüller
-            </a>
-          </li>
-        </ul>
-        <h2 className="h2">Free Courses</h2>
-        <h3 className="h3">JavaScript</h3>
-        <ul>
-          <li>
-            <a
-              href="https://JavaScript30.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JavaScript 30 by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/watch?v=W6NZfCO5SIk"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              What is JavaScript by Mosh
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/watch?v=hdI2bqOjy3c"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JavaScript Crash Course by Brad Traversy
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">React</h3>
-        <ul>
-          <li>
-            <a
-              href="https://www.youtube.com/watch?v=sBws8MSXN7A&t=1s"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Reactjs Crash Course by Brad Traversy
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/watch?v=Ke90Tje7VS0"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Reactjs Crash Course by Mosh
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/watch?v=DLX62G4lc44"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React JS from Free Code Camp
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">CSS</h3>
-        <ul>
-          <li>
-            <a
-              href="https://CSSGrid.io/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CSS Grid by Wes Bos
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://Flexbox.io/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              What the Flexbox by Wes Bos
-            </a>
-          </li>
-        </ul>
-        <h3 className="h3">Misc</h3>
-        <ul>
-          <li>
-            <a
-              href="https://CommandLinePowerUser.com/friend/QUICK"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Command Line Power User by Wes Bos
-            </a>
-          </li>
-        </ul>
-        <h2>Premium Subscription Sites</h2>
-        <ul>
-          <li>
-            <a
-              href="https://www.leveluptutorials.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Level Up Tutorials by Scott Tolinski
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://frontendmasters.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Frontend Masters
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://egghead.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Egg Head
-            </a>
-          </li>
-        </ul>
-        <h2>People To Follow</h2>
-        <li>
-          <a
-            href="https://twitter.com/wesbos"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Wes Bos
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/stolinski"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Scott Tolinski
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/laurieontech"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Laurie on Tech
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/EmmaBostian"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Emma Bostian
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/ASpittel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ali Spittel
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://twitter.com/jsjoeio"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            JavaScript/TypeScript Joe
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/brob"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Bryan Robinson
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/sarah_edo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sarah Drasner
-          </a>
-        </li>
-        <h2>Podcasts to Listen To</h2>
-        <ul>
-          <li>
-            <a
-              href="https://scotch.io/bar-talk/top-10-podcasts-for-web-developers#toc-syntax"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Syntax by Wes Bos and Scott Tolinski
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://blog.codepen.io/radio/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code Pen Radio
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://www.fullstackradio.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Full Stack Radio
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://shoptalkshow.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Shop Talk Radio
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://devchat.tv/js-jabber/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JavaScript Jabber
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://reactpodcast.simplecast.fm/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Podcast
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.codenewbie.org/podcast"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code Newbie
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://frontendhappyhour.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Front End Happy Hour
-            </a>
-          </li>
-        </ul>
       </section>
+      {typesArr.map((type, index) => (
+        <section key={index}>
+          <h3 className="h3">{type}</h3>
+          <ul>
+            {resources
+              .filter(resource => resource.type[0].title === type)
+              .map((resource, i) => (
+                <p key={i}>
+                  <a
+                    href={resource.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {resource.title}
+                  </a>
+                </p>
+              ))}
+          </ul>
+        </section>
+      ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query ResourcesQuery {
+    allSanityResource {
+      nodes {
+        _id
+        title
+        tag {
+          title
+        }
+        link
+        type {
+          title
+        }
+      }
+    }
+  }
+`;
