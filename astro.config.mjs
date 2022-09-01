314580f2798e93cd7eddf7700beabc7799be4324
jamesqquick <james.q.quick@gmail.com>
2022-08-31 19:22:53 -0500

    Removed astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 import partytown from "@astrojs/partytown";
 
