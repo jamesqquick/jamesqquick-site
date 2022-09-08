a5035015ea34f5c58b5c1530ffa09129625aca4f
jamesqquick <james.q.quick@gmail.com>
2022-09-08 12:38:21 -0500

    Small google analytics updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,17 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jamesqquick.com/",
-  integrations: [mdx(), sitemap({}), tailwind(), partytown(), image()],
-  output: "static"
+  integrations: [
+    mdx(),
+    sitemap({}),
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    image(),
+  ],
+  output: "static",
 });
