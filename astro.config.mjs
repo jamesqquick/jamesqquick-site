0ae1839d8f6c78121466ce442e45cbae23e74e62
jamesqquick <james.q.quick@gmail.com>
2023-04-19 14:48:28 -0500

    Fixed non-loading mage on hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte"; // https://astro.build/config
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jamesqquick.com/",
-  integrations: [mdx(), sitemap({}), tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), svelte()],
+  integrations: [
+    mdx(),
+    sitemap({}),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    svelte(),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
