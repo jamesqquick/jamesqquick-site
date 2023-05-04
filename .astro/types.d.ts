762b47b364f0ccde5e2cfef692115e8def433735
jamesqquick <james.q.quick@gmail.com>
2023-05-04 10:32:25 -0500

    Adding openai nextjs blog post

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -167,6 +167,13 @@ declare module 'astro:content' {
   collection: "blog",
   data: InferEntrySchema<"blog">
 } & { render(): Render[".md"] },
+"2023-05-04-how-to-add-openai-to-your-nextjs-project.md": {
+  id: "2023-05-04-how-to-add-openai-to-your-nextjs-project.md",
+  slug: "add-openai-nextjs-project",
+  body: string,
+  collection: "blog",
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] },
 "5-things-i-ve-learned-from-creating-youtube-videos.md": {
   id: "5-things-i-ve-learned-from-creating-youtube-videos.md",
   slug: "5-things-i-ve-learned-from-creating-youtube-videos",
