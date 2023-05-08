efe108545abaf0b1f64a44b2ddcc62fbed4d8a2f
jamesqquick <james.q.quick@gmail.com>
2023-05-08 10:57:02 -0500

    Added content to xml feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,8 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export const get = async () => {
   const blogs = await getCollection("blog");
@@ -10,12 +12,13 @@ export const get = async () => {
     pubDate: post.data.pubDate,
     description: post.data.description,
     link: `/blog/${post.slug}/`,
+    content: sanitizeHtml(parser.render(post.body)),
   }));
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    content: sanitizeHtml(parser.render(post.body)),
+
     items,
   });
 };
