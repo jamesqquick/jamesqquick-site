7c3f628a9c7b9753bcac1e37cb5037a169d13ae9
jamesqquick <james.q.quick@gmail.com>
2023-05-08 10:46:50 -0500

    Fixed rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,19 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
 
-export const get = () =>
-  rss({
+export const get = async () => {
+  const blogs = await getCollection("blog");
+  const items = blogs.map((post) => ({
+    title: post.data.title,
+    pubDate: post.data.pubDate,
+    description: post.data.description,
+    link: `/blog/${post.slug}/`,
+  }));
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./blog/**/*.md'),
+    items,
   });
+};
