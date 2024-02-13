b77d22484b47f0c4c7c083ba65983cadeafde529
jamesqquick <james.q.quick@gmail.com>
2024-02-13 10:44:15 -0600

    Updated Netlify adapter and added sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,20 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
 
-// import sentry from "@sentry/astro";
-// import spotlightjs from "@spotlightjs/astro";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jamesqquick.com/",
-  integrations: [mdx(), sitemap({}), tailwind(), svelte()],
+  integrations: [
+    mdx(),
+    sitemap({}),
+    tailwind(),
+    svelte(),
+    sentry(),
+    spotlightjs(),
+  ],
   output: "server",
   adapter: netlify(),
 });
