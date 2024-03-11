35e82cb081ef24d322644d13933b53f8e83ab9e4
jamesqquick <james.q.quick@gmail.com>
2024-03-11 11:48:18 -0500

    Comment out source maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
+import spotlightjs from "@spotlightjs/astro";
 
 import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,14 +16,14 @@ export default defineConfig({
     sitemap({}),
     tailwind(),
     svelte(),
+    spotlightjs(),
     sentry({
-      dsn: process.env.SENTRY_DSN,
+      dsn: "https://ab69a01d3403b79bf1b71fdce18be5a7@o4506232146034688.ingest.us.sentry.io/4506384553869312",
       //   sourceMapsUploadOptions: {
       //     project: "james-q-quick-website",
       //     authToken: process.env.SENTRY_AUTH_TOKEN,
       //   },
     }),
-    spotlightjs(),
   ],
   output: "server",
   adapter: netlify(),
