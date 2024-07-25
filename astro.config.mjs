ba4512644bd6937996927e24ae433fff785482d3
jamesqquick <james.q.quick@gmail.com>
2024-07-25 12:32:48 -0500

    Upgraded versions of astro and extensions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 import svelte from "@astrojs/svelte";
 import spotlightjs from "@spotlightjs/astro";
-
 import sentry from "@sentry/astro";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,6 +25,21 @@ export default defineConfig({
       //   },
     }),
     spotlightjs(),
+    icon({
+      include: {
+        // Include only three `mdi` icons in the bundle
+        mdi: [
+          "close",
+          "account-plus",
+          "arrow-left",
+          "arrow-right",
+          "menu",
+          "account",
+          "laptop",
+          "star-box",
+        ],
+      },
+    }),
   ],
   output: "server",
   adapter: netlify(),
