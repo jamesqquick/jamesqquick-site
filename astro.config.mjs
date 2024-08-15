636bd1e4d38354260306b05f08c4b1a8ea6912d1
jamesqquick <james.q.quick@gmail.com>
2024-08-15 09:01:18 -0500

    Added selfie wall

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,9 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import svelte from "@astrojs/svelte";
-import spotlightjs from "@spotlightjs/astro";
-import sentry from "@sentry/astro";
-
 import icon from "astro-icon";
+
+import tailwindConfigViewer from "astro-tailwind-config-viewer";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,13 +16,6 @@ export default defineConfig({
     sitemap({}),
     tailwind(),
     svelte(),
-    // sentry({
-    //   dsn: "https://ab69a01d3403b79bf1b71fdce18be5a7@o4506232146034688.ingest.us.sentry.io/4506384553869312",
-    //   //   sourceMapsUploadOptions: {
-    //   //     project: "james-q-quick-website",
-    //   //     authToken: process.env.SENTRY_AUTH_TOKEN,
-    //   //   },
-    // }),
     icon({
       include: {
         // Include only three `mdi` icons in the bundle
@@ -40,6 +32,7 @@ export default defineConfig({
         ],
       },
     }),
+    tailwindConfigViewer(),
   ],
   output: "server",
   adapter: netlify(),
